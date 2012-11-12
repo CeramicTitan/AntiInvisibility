d4e14e11391ef8278603a212f9538b8ce3018bcb
Trent Hensler <thensler@wisc.edu>
2012-11-12 15:47:53 -0600

    Not sure
    Tried fixing bug.

--- src/mc/shock/antiInvisible/TopDog.java
+++ src/mc/shock/antiInvisible/TopDog.java
@@ -2,6 +2,7 @@ package mc.shock.antiInvisible;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,25 +16,30 @@ import org.bukkit.potion.PotionEffectType;
 
 public class TopDog extends JavaPlugin implements Listener{
 
+	public FileConfiguration config;
+	
 	public void onEnable()
 	{
 		getLogger().info("Anti Invisible enabled!");
 		getServer().getPluginManager().registerEvents(this, this);
+		config = getConfig();
+		config.set("AdminsSeePlayers", false);
+		saveConfig();
 	}
 	public void onDisable()
 	{
 		getLogger().info("Anti Invisible disabled!");
+		saveConfig();
 	}
-	
-	
-	
 	
 	
 	@EventHandler
 	public void onPlayerClick(PlayerInteractEvent e)
 	{
+		final int timer1 = 10;
+		Boolean adminsee = getConfig().getBoolean("AdminsSeePlayers");
 		Action action = e.getAction();
-		Player player = (Player) e.getPlayer();
+		final Player player = (Player) e.getPlayer();
 		if (((action == Action.RIGHT_CLICK_AIR) || (action == Action.RIGHT_CLICK_BLOCK)))
 		{
 			ItemStack it = player.getItemInHand();
@@ -44,14 +50,34 @@ public class TopDog extends JavaPlugin implements Listener{
 				PotionEffectType effecttype = potion.getType().getEffectType();
 				if (effecttype == PotionEffectType.INVISIBILITY)
 				{
+					if (adminsee == true)
+					{
 			       e.setCancelled(true);
 			       player.getInventory().setItemInHand(new ItemStack (Material.CAKE, 1));
 			       player.getPlayer().sendMessage(ChatColor.RED + "Potions of invisibility are disabled.");
 			       player.getPlayer().sendMessage(ChatColor.RED + "But here's some cake :D");
+					}
+					else
+					{
+						e.setCancelled(true);
+						player.hidePlayer(player);
+						this.getServer().getScheduler().scheduleAsyncDelayedTask(this, new Runnable()
+						{
+							public void run()
+							{
+								if (timer1 != 10)
+								{
+									Player allplayers = (Player) getServer();
+									player.hidePlayer(allplayers);
+									timer1++;
+								}
+							}
+						}
+					}
+				}
+		    }
 		}
-		}
-		}
-		}	
+	}	
 	
 	   @EventHandler
 	    public static PlayerInteractEvent interact(PlayerInteractEvent event)
